6da2601bc011aeeca1e6adf509ea7cf895bca070
rafaelabittencourt <rafaela.bittencourt@ambevtech.com.br>
2021-08-20 09:28:27 -0300

    javascript ex 2 two-fer

--- teams/start-tech/javascript/two-fer/two-fer.spec.js
+++ teams/start-tech/javascript/two-fer/two-fer.spec.js
@@ -4,7 +4,13 @@ describe('twoFer()', () => {
   test('no name given', () => {
     expect(twoFer()).toEqual('One for you, one for me.');
   });
+  test('empty name given', () => {
+    expect(twoFer('')).toEqual('One for you, one for me.');
+  });
 
+  test('null name given', () => {
+    expect(twoFer(null)).toEqual('One for you, one for me.');
+  });
   test('a name given', () => {
     expect(twoFer('Alice')).toEqual('One for Alice, one for me.');
   });
@@ -13,3 +19,4 @@ describe('twoFer()', () => {
     expect(twoFer('Bob')).toEqual('One for Bob, one for me.');
   });
 });
+
