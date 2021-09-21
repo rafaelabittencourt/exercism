73fa18b3b03260a5f24db3a4128a13887a54e346
rafaelabittencourt <rafaela.bittencourt@ambevtech.com.br>
2021-09-21 13:00:48 -0300

    ex 14

---
+++ javascript/factory-sensors/factory-sensors.js
@@ -0,0 +1,39 @@
+export class ArgumentError extends Error {}
+
+export class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
+}
+
+export function checkHumidityLevel(humidityPercentage) {
+  if (humidityPercentage > 70) {
+    throw new Error(`The humidity percentage is ${humidityPercentage} ! That's too high !`);
+  }
+}
+
+export function reportOverheating(temperature) {
+  if (temperature === null) {
+    throw new ArgumentError(`The sensor is broken.`);
+  } else if (temperature < 500) {
+    return;
+  }
+    throw new OverheatingError(temperature);
+}
+
+export function monitorTheMachine({check, alertDeadSensor, alertOverheating, shutdown}) { 
+  try { 
+    check();
+  } catch (error) {
+    if (error instanceof ArgumentError) { 
+      alertDeadSensor(); 
+    } else if (error instanceof OverheatingError) {  
+      if (error.temperature > 600) { shutdown(); 
+      } else { alertOverheating() };
+    }
+    else {
+      throw error;
+    }
+  }
+}
