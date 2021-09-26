f1fe243687da3e2e6eaf5c590ed433d3cde42bb5
rafaelabittencourt <rafaela.bittencourt@ambevtech.com.br>
2021-09-26 15:39:24 -0300

    ex 18

---
+++ javascript/elyses-analytic-enchantments/enchantments.js
@@ -0,0 +1,27 @@
+export function getCardPosition(stack, card) {
+  return stack.indexOf(card);
+}
+
+export function doesStackIncludeCard(stack, card) {
+  return stack.includes(card);
+}
+
+export function isEachCardEven(stack) {
+  return stack.every((card) => card % 2 === 0);
+  /* mesma coisa que:
+  return stack.every(function isEven(card) {
+    if (card % 2 === 0) return true;
+  }); */
+}
+
+export function doesStackIncludeOddCard(stack) {
+  return stack.some((card) => card % 2 !== 0);
+}
+
+export function getFirstOddCard(stack) {
+  return stack.find((card) => card % 2 !== 0);
+}
+
+export function getFirstEvenCardPosition(stack) {
+  return stack.findIndex((card) => card % 2 === 0);
+}
