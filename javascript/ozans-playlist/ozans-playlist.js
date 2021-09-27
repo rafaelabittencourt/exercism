f8c07a09a8d796b79803fdcf981f7b65962f3451
rafaelabittencourt <rafaela.bittencourt@ambevtech.com.br>
2021-09-27 14:58:42 -0300

    ex 20

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,28 @@
+export function removeDuplicates(playlist) {
+  return [...new Set(playlist)];
+}
+
+export function hasTrack(playlist, track) {
+  let mySet = new Set(playlist);
+  return mySet.has(track);
+}
+
+export function addTrack(playlist, track) {
+  let mySet = new Set(playlist);
+  let newSet = mySet.add(track);
+  return [...newSet];
+}
+
+export function deleteTrack(playlist, track) {
+  let mySet = new Set(playlist);
+  let inPlaylist = mySet.delete(track);
+  return [...mySet];
+}
+
+export function listArtists(playlist) {
+  const newArr = [];
+  for (let song of playlist) {
+    newArr.push(song.split(' - ')[1]); 
+  }
+  return [...new Set(newArr)];
+}
